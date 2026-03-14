30407cf5c52a47e69a6a006112e5fc2478fb87ba
Sanjana <sanjana.sureshkumar05@gmail.com>
2026-03-14 10:34:31 +0530

    logging config

---
+++ bot/logging_config.py
@@ -0,0 +1,30 @@
+import logging
+from pathlib import Path
+
+
+def setup_logger() -> logging.Logger:
+    log_dir = Path("logs")
+    log_dir.mkdir(exist_ok=True)
+
+    logger = logging.getLogger("trading_bot")
+    logger.setLevel(logging.INFO)
+
+    if logger.handlers:
+        return logger
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
+    file_handler = logging.FileHandler(log_dir / "trading_bot.log", encoding="utf-8")
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(formatter)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
